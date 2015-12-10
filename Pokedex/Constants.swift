58c65538e2090e471ba466d12d56e875b04137e5
yarenty <yarenty@gmail.com>
2015-12-10 16:56:02 +0000

    constants

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Jaroslaw Nowosad on 10/12/2015.
+//  Copyright © 2015 SkyCorp Ltd. All rights reserved.
+//
+
+import Foundation
+
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+// own custom clojure 
+typealias DownloadComplete = () -> ()
+
